18d18e112fac94bb78d676128e8b00e7209d5722
pmb087 <107172997+pmb087@users.noreply.github.com>
2022-10-05 10:17:41 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 36.66 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,11 @@
+function solution(arr)
+{
+    let answer = [10];    
+    
+    for(let i = 0; i < arr.length; i++){
+        if(answer[answer.length - 1] != arr[i]) answer.push(arr[i]);
+    }
+    answer.shift();
+    
+    return answer;
+}
