08f527716caa9d66f0c9eeab0206cb09128b1fcc
박승민 - SeugnMin Park <107172997+pmb087@users.noreply.github.com>
2022-12-28 14:17:14 +0900

    [level 2] Title: 예상 대진표, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,13 @@
+function solution(n,a,b){
+    let answer = 1;
+    if((a === 1 && b === 2) || (a % 2 === 1 && a + 1 === b) || (b % 2 === 1 && b + 1 === a)) return answer;
+    while(Math.max(a,b) - 1 !== Math.min(a,b) || Math.max(a,b) % 2 === 1){
+        a = Math.ceil(a / 2);
+        b = Math.ceil(b / 2);
+        answer++;
+    }
+    if(a / 2 >= 1 && a % 2 === 0){
+        
+    }
+    return answer === 1 ? 2 : answer;
+}
