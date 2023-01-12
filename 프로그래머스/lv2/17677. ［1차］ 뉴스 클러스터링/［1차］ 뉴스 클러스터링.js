34c54792194013ade5a9fe9848ca5c49c7d6da8d
박승민 - SeugnMin Park <107172997+pmb087@users.noreply.github.com>
2023-01-13 08:34:32 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,32 +1,26 @@
-function solution(str1, str2) {
-
-    const s1 = str1.toLowerCase();
-    const s2 = str2.toLowerCase();
-
-    var eng = /[a-z]/g;
+function format(arg){
+    const lowerArg = arg.toLowerCase();    
+    const eng = /[a-z]/g;
+    const arr = [];
     
-    const arr1 = [];
-    const arr2 = [];
-    for(let i =0;i<s1.length-1;i++) {
-        const str = s1[i] + s1[i+1];
-        if(str.replace(eng, '').length == 0) arr1.push(str);
+     for(let i = 0; i < lowerArg.length-1; i++) {
+        const str = lowerArg[i] + lowerArg[i+1];
+        if(str.replace(eng, '').length === 0) arr.push(str);
     }
-    for(let i =0;i<s2.length-1;i++) {
-        const str = s2[i] + s2[i+1];
-        if(str.replace(eng, '').length == 0) arr2.push(str);
-    }
+    return arr;
+}
+
+function solution(str1, str2) {
+    const arr1 = format(str1);
+    const arr2 = format(str2);
     
     if(arr1.length === 0 && arr2.length=== 0) return 65536;
-    
-    // arr1.sort();
-    // arr2.sort();
-    // if(JSON.stringify(arr1) == JSON.stringify(arr2)) return 65536;
 
-    const tempArr2 = JSON.parse(JSON.stringify(arr2));
+    const instance = [...arr2];
     const intersection = arr1.reduce((acc,cur)=> {
-        if(tempArr2.includes(cur)) {
-            const index2 = tempArr2.indexOf(cur);
-            tempArr2.splice(index2, 1);
+        if(instance.includes(cur)) {
+            const index2 = instance.indexOf(cur);
+            instance.splice(index2, 1);
             return [...acc, cur];
         } else {
             return acc;
@@ -34,5 +28,5 @@ function solution(str1, str2) {
     },[]);
     
     const union = arr1.length + arr2.length - intersection.length;
-    return Math.floor(intersection.length/union*65536);
+    return Math.floor(intersection.length / union * 65536);
 }
