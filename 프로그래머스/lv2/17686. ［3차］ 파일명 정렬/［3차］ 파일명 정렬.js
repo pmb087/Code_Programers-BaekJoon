397d0114ab7b22bd63e70ca16175b5fd4a1e99b0
박승민 - SeugnMin Park <107172997+pmb087@users.noreply.github.com>
2023-02-03 13:16:46 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 3.25 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -0,0 +1,10 @@
+const solution = (files) => {
+        return files
+          .map((file) => file.match(/(\D+)(\d+)/))
+          .sort((a, b) => {
+            if (a[1].toLowerCase() > b[1].toLowerCase()) return 1;
+            else if (a[1].toLowerCase() < b[1].toLowerCase()) return -1;
+            else return parseInt(a[2]) - parseInt(b[2]);
+          })
+          .map((file) => file.input);
+      };
