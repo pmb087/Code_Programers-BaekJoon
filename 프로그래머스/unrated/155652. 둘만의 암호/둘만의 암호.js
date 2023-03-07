888a9b86f645dd8dd29280d0fc728862590a44dd
박승민 - SeugnMin Park <107172997+pmb087@users.noreply.github.com>
2023-03-07 15:34:48 +0900

    [unrated] Title: 둘만의 암호, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,16 @@
+function solution(s, skip, index) {
+    s = s.split('').map((_,i) => s.charCodeAt(i));
+    skip = skip.split('').map((_,i) => skip.charCodeAt(i)).sort((a,b)=>a-b);
+    
+    s = s.map(Oel => {
+        let current = Oel;
+        for(let i = 0; i < index; i++){
+            current++
+            if(current > 122) current = 97;
+            if(skip.includes(current)) i--;
+        }
+        return current;
+    })
+    
+    return String.fromCharCode(...s)
+}
