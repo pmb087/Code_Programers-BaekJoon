e798017de822fee1b8a5aa8bda8b0c10b02191a6
박승민 - SeugnMin Park <107172997+pmb087@users.noreply.github.com>
2023-04-25 15:33:25 +0900

    [level 0] Title: 약수 구하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    const answer = [];
+    
+    for(let i = 1; i <= (n / 2); i++){
+        if(n % i === 0) answer.push(i);
+    }
+    
+    answer.push(n);
+    
+    return answer;
+}
