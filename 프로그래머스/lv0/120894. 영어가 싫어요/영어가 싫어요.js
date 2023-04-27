01cec00218ca25f4279696412615918cdc812c97
박승민 - SeugnMin Park <107172997+pmb087@users.noreply.github.com>
2023-04-27 14:35:26 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,14 @@
+function solution(numbers) {
+    let answer = numbers.replace(/one/g,'1');
+    answer = answer.replace(/two/g,'2');
+    answer = answer.replace(/three/g,'3');
+    answer = answer.replace(/four/g,'4');
+    answer = answer.replace(/five/g,'5');
+    answer = answer.replace(/six/g,'6');
+    answer = answer.replace(/seven/g,'7');
+    answer = answer.replace(/eight/g,'8');
+    answer = answer.replace(/nine/g,'9');
+    answer = answer.replace(/zero/g,'0');
+    
+    return parseInt(answer)
+}
